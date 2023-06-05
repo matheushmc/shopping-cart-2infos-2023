109fc930dfdb35554bf8332a1f4332b7e66943e0
matheushmc <matheus20carvalhoteteu@gmail.com>
2023-06-05 11:50:35 -0300

    feito

---
+++ src/componentes/listagemlivros.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { livros } from '@/_data/livros.js'
+
+import cardlivro from '@/componentes/cardlivro.vue';
+
+</script>
+
+
+<template>
+ <div class="listagem-livros">
+     
+
+      <cardlivro v-for="livro in livros" :key="livro.id" :livro="livro"/>
+
+
+    </div>
+
+</template>
+
+<style scoped>
+.listagem-livros {
+  display: flex;
+  flex-wrap: wrap;
+}
+</style>
